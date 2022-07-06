42d50e4f97ac9c17e1efcf1f04f9ab494fa27de5
StenKa <stepansergeevichz13@gmail.com>
2022-07-06 23:21:59 +0300

    Выполнена четвертая задача

---
+++ Task 4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= number){
+    Console.Write(current + " ");
+    current = current + 2;
+}
